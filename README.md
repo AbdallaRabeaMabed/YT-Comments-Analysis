e4552a6070e9233be569169c155ca3d73ba0ebc4
Abdalla Rabea Mabed <abdallamabed407@gmail.com>
2026-01-19 01:22:07 +0200

    Initial clean commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# YT-Comments-Analysis
+# perform sentiment analysis and key phrase extraction on YouTube comments using Azure Cognitive Services – Text Analytics API.
+---
+Tools
+-   export comments Website 
+-	Azure Cognitive Services – Text Analytics
+-	Python (Pandas, Requests, OpenPyXL)
+-	Excel / Google Sheets
+-	Power BI  for dashboard and visualization
+
+-----
+![Dashboard](Dashboard.png)
+
---
+++ Script.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import requests
+import time
+
+df = pd.read_excel("yt-comments_ROLhrYkUQhc_9c424ae3.xlsx")
+
+comments_column = "Comment"
+
+#add your key and endpoint values here
+api_key =""
+endpoint = ""
+
+sentiment_url = endpoint + "text/analytics/v3.1/sentiment"
+keyphrase_url = endpoint + "text/analytics/v3.1/keyPhrases"
+
+headers = {
+    "Ocp-Apim-Subscription-Key": api_key,
+    "Content-Type": "application/json"
+}
+
+
+sentiments = []
+key_phrases_list = []
+
+for i, comment in enumerate(df[comments_column]):
+    if pd.isna(comment) or str(comment).strip() == "":
+        sentiments.append("Empty")
+        key_phrases_list.append("")
+        continue
+
+    doc = {
+        "documents": [
+            {
+                "id": str(i),
+                "language": "ar", 
+                "text": str(comment)
+            }
+        ]
+    }
+
+    
+    sent_resp = requests.post(sentiment_url, headers=headers, json=doc)
+    if sent_resp.status_code == 200:
+        sentiment = sent_resp.json()["documents"][0]["sentiment"]
+    else:
+        sentiment = "Error"
+
+   
+    key_resp = requests.post(keyphrase_url, headers=headers, json=doc)
+    if key_resp.status_code == 200:
+        key_phrases = key_resp.json()["documents"][0]["keyPhrases"]
+        key_phrases = ", ".join(key_phrases)
+    else:
+        key_phrases = "Error"
+
+    sentiments.append(sentiment)
+    key_phrases_list.append(key_phrases)
+
+    time.sleep(0.5) 
+
+
+df["Sentiment"] = sentiments
+df["Key Phrases"] = key_phrases_list
+
+df.to_excel("analyzed_comments_with_keywords.xlsx", index=False)
+
+print("Done! File saved as 'analyzed_comments_with_keywords.xlsx'")
+
